ef0f20a8eae2f5722842838a58a0e51753325085
Computerquiziz <kandpaldeepa57@gmail.com>
2026-07-23 16:43:41 +0530

    Add quiz questions to questions.js

---
+++ questions.js
@@ -0,0 +1,102 @@
+const questions = [
+{
+question: "HTML stands for?",
+options: [
+"Hyper Text Markup Language",
+"High Text Machine Language",
+"Hyper Transfer Markup Language",
+"Home Tool Markup Language"
+],
+answer: 0
+},
+{
+question: "CSS is used for?",
+options: [
+"Programming",
+"Styling Web Pages",
+"Database",
+"Networking"
+],
+answer: 1
+},
+{
+question: "JavaScript is a?",
+options: [
+"Programming Language",
+"Database",
+"Browser",
+"Operating System"
+],
+answer: 0
+},
+{
+question: "Which tag is used for the largest heading?",
+options: [
+"<h6>",
+"<h1>",
+"<head>",
+"<title>"
+],
+answer: 1
+},
+{
+question: "Which tag inserts a line break?",
+options: [
+"<br>",
+"<hr>",
+"<p>",
+"<b>"
+],
+answer: 0
+},
+{
+question: "Which attribute is used for image path?",
+options: [
+"href",
+"src",
+"alt",
+"link"
+],
+answer: 1
+},
+{
+question: "Which CSS property changes text color?",
+options: [
+"background",
+"font-size",
+"color",
+"text-style"
+],
+answer: 2
+},
+{
+question: "Which symbol is used for ID selector in CSS?",
+options: [
+".",
+"#",
+"*",
+"&"
+],
+answer: 1
+},
+{
+question: "JavaScript code is written inside?",
+options: [
+"<css>",
+"<script>",
+"<style>",
+"<body>"
+],
+answer: 1
+},
+{
+question: "Which keyword declares a variable in JavaScript?",
+options: [
+"var",
+"int",
+"string",
+"define"
+],
+answer: 0
+}
+];
